eb54d58c7bce8def46f955301132486ee33652c4
Igor Rodeckiy <radeckiy777@gmail.com>
2022-07-05 18:26:13 +0300

    changes

--- server/app.js
+++ server/app.js
@@ -7,7 +7,6 @@ const port = +process.env.PORT || 8080,
 app.use(cors())
 app.use(express.urlencoded({ limit: '50mb', extended: true }))
 app.use(express.json({ limit: '50mb' }))
-app.use(express.static(path.join(__dirname, '../public')))
 app.use(express.static(path.join(__dirname, '../build')))
 app.get('/todos', (req, res) => res.sendFile(path.join(__dirname, 'todos.json')))
 app.get('*', (req, res) => {
